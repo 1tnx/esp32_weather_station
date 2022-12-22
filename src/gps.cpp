7079b3b905863a8a28f70244a6f5f3d3dfe96b27
Lancelot Neirinckx <73384301+1tnx@users.noreply.github.com>
2022-12-22 11:00:12 +0100

    modify gps code

--- src/gps.cpp
+++ src/gps.cpp
@@ -1,21 +1,33 @@
 #include "gps.h"
 
-#define GPS_SERIAL Serial1
+#define GPS_RX 18
+#define GPS_TX 17
+
+HardwareSerial serial(1);
 
 void GPS::init() {
-    GPS_SERIAL.begin(9600);
+    serial.begin(9600, SERIAL_8N1, GPS_RX, GPS_TX);
 }
 
 void GPS::update() {
+    while (serial.available() > 0) {
+        gps.encode(serial.read());
+    }
     if (millis() - lastUpdate > updateInterval) {
-        while (GPS_SERIAL.available() > 0) {
-            gps.encode(GPS_SERIAL.read());
-        }
-
         if (gps.location.isValid()) {
+            lastUpdate = millis();
             latitude = gps.location.lat();
             longitude = gps.location.lng();
-            lastUpdate = millis();
+        }
+        if (gps.date.isValid()) {
+            day = gps.date.day();
+            month = gps.date.month();
+            year = gps.date.year();
+        }
+        if (gps.time.isValid()) {
+            second = gps.time.second();
+            minute = gps.time.minute();
+            hour = gps.time.hour();
         }
     }
 }
@@ -26,4 +38,14 @@ double GPS::getLatitude() {
 
 double GPS::getLongitude() {
     return longitude;
+}
+
+char* GPS::getDateTime() {
+    char* dateTime = new char[23];
+    sprintf(dateTime, "%02d/%02d/%02d %02d:%02d:%02d", day, month, year, hour, minute, second);
+    return dateTime;
+}
+
+unsigned long GPS::getLastUpdate() {
+    return lastUpdate;
 }
--- src/gps.h
+++ src/gps.h
@@ -4,15 +4,22 @@ class GPS {
     TinyGPSPlus gps;
     double latitude;
     double longitude;
+    byte second;
+    byte minute;
+    byte hour;
+    byte day;
+    byte month;
+    int year;
     unsigned long lastUpdate;
     unsigned long updateInterval;
-
     public:
-        GPS(unsigned long interval) {
-            updateInterval = interval;
+        GPS(unsigned long updateInterval) {
+            this->updateInterval = updateInterval;
         }
         void init();
         void update();
         double getLatitude();
         double getLongitude();
+        char* getDateTime();
+        unsigned long getLastUpdate();
 };
